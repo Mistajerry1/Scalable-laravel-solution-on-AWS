10cbe3b8bd0a6429e81fcf518c4c1d357899f223
Mistajerry1 <168672771+Mistajerry1@users.noreply.github.com>
2024-08-11 19:15:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "aws_vpc_security_group_ingress_rule" "allowssh" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.rule_laravel.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1" 
 }
 
 
